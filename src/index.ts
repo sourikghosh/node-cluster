6b096907aa4d740bd689307de1b3c7eb130ca714
Sourik Ghosh <sourikghosh31@gmail.com>
2021-01-12 14:29:30 +0530

    updated the computation

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import express from 'express'
 import cluster from 'cluster'
 import { cpus } from 'os'
-import { heavyTask } from './heavyTask'
+import { hash } from 'bcrypt'
 
 const app = express()
-
 const numCpu = cpus().length
 
-app.get('/', (req, res, next) => {
-    heavyTask()  //some heavy task
+app.get('/', async (req, res, next) => {
+    const Password = '1e1055fb1399691f7cb69b0c55a90a09442f5b47c2997a5e4b489f85302d743f71e5'
+    const hPassword = await hash(Password, 14)
     res.send({
         status: 'ok',
-        process_id: process.pid
+        password: hPassword
     })
 })
 
